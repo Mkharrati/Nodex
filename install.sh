3d343d2c49be4b34970b409fc93088f299eaed5b
Emad <152497690+Mkharrati@users.noreply.github.com>
2024-09-20 01:58:16 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo cd
 sudo rm Nodex.sh
+sudo rm /usr/bin/Nodex
 sudo wget https://github.com/Mkharrati/Nodex/raw/refs/heads/main/Nodex.sh
 sudo mv Nodex.sh /usr/bin/Nodex
 sudo chmod 777 /usr/bin/Nodex
+clear
+echo "Nodex Installed successfuly."
